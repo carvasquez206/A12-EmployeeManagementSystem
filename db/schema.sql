8513f786ec0436a24fc549535c855fb1abe1a581
Carlos <carlos96831@gmail.com>
2022-01-27 23:20:07 -0800

    adding info to db/schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,17 +5,23 @@ USE employee_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  book_name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE department (
+CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  book_name VARCHAR(30) NOT NULL
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL  NOT NULL, 
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) References (department_id) ON DELETE SET NULL
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  book_name VARCHAR(30) NOT NULL
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+
 );
 
 /* `department`
