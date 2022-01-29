bffd72586660e4a43695c1644bfe0f0743fb7f56
Carlos <carlos96831@gmail.com>
2022-01-28 17:37:41 -0800

    added an image to print when starting sql server

--- server.js
+++ server.js
@@ -6,3 +6,28 @@ require('dotenv').config();
 // Import console.table
 const consoleTable = require('console.table')
 
+
+// connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.MYSQL_PASSWORD,
+    database: 'employee_db'
+  });
+  
+  connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    afterConnection();
+  });
+  
+  // welcome image
+  afterConnection = () => {
+    console.log("***********************************")
+    console.log("*                                 *")
+    console.log("*           Moshi! Moshi!         *")
+    console.log("*                                 *")
+    console.log("***********************************")
+    promptUser();
+  };
+  
